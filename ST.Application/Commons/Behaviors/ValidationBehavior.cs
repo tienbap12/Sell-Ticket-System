53cb3f49a5e6f020860ea8f3f0ee07adc5222696
Tien Ngo <tienbap12@gmail.com>
2024-04-04 10:34:01 +0700

    update behavior

--- ST.Application/Commons/Behaviors/ValidationBehavior.cs
+++ ST.Application/Commons/Behaviors/ValidationBehavior.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using FluentValidation;
+﻿using FluentValidation;
 using MediatR;
 
 namespace ST.Application.Commons.Behaviors
